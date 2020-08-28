e1ca795559c5c555a91bd42cb1feec8ea8803f7f
Lori Shepherd <lori.shepherd@roswellpark.org>
2020-08-28 07:48:45 -0400

    Fix onAttach message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onAttach <- function(...) {
+.onAttach <- function(libname, pkgname) {
     msg <- sprintf(
         "Package '%s' is deprecated and will be removed from Bioconductor
          version %s", pkgname, "3.13")
